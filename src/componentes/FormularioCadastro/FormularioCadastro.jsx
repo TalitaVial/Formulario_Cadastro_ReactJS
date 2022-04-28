25a553bbf01a998a55a6a197df9794aa78cd0a1e
Talita Vial <talitavialsilva1@gmail.com>
2022-04-27 21:57:48 -0400

    feat: SubmitForm

--- src/componentes/FormularioCadastro/FormularioCadastro.jsx
+++ src/componentes/FormularioCadastro/FormularioCadastro.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import {Button, TextField, Switch, FormControlLabel} from '@material-ui/core'
 
-
-
 function FormularioCadastro() {
+  let nome = ''
   return (
-    <form>
-      <TextField id='nome' label='Nome' variant='outlined' fullWidth margin='normal'/>
+    <form onSubmit={(event)=>{
+      event.preventDefault()
+      console.log(nome)
+    }}>
+      <TextField onChange={(event)=>{
+        nome = event.target.value
+       
+      }} id='nome' label='Nome' variant='outlined' fullWidth margin='normal'/>
       <TextField id='sobrenome' label='Sobrenome' variant='outlined' fullWidth margin='normal'/>
       <TextField id='CPF' label='CPF' variant='outlined' fullWidth margin='normal'/>
       <FormControlLabel label='Promoções' control={<Switch defaultChecked color='primary' />}/>
       <FormControlLabel label='Novidades' control={<Switch defaultChecked color='primary' />}/>
-      <Button variant='contained' color='primary'>cadastrar</Button>
+      <Button type='submit' variant='contained' color='primary'>cadastrar</Button>
     </form>
   );
 }
