d548672d71257684d5f7a06d5119b817a5ee58d3
Talita Vial <talitavialsilva1@gmail.com>
2022-05-09 20:43:43 -0400

    feat: capturando o estado dos campos de nome, sobrenome, cpf

--- src/componentes/FormularioCadastro/FormularioCadastro.jsx
+++ src/componentes/FormularioCadastro/FormularioCadastro.jsx
@@ -4,6 +4,9 @@ import {Button, TextField, Switch, FormControlLabel} from '@material-ui/core'
 function FormularioCadastro() {
   const [nome, setNome] = useState("")
   const [sobrenome, setSobrenome] = useState("")
+  const [cpf, setCpf] = useState("")
+  // const [promocoes, setPromocoes] = useState("")
+  // const [novidades, setNovidades] = useState("")
 
   return (
     <form onSubmit={(event)=>{
@@ -11,18 +14,15 @@ function FormularioCadastro() {
       console.log(nome, sobrenome)
     }}>
       <TextField value={nome} onChange={(event)=>{
-        let tpmNome = event.target.value
-        if(tpmNome.length >= 3){
-          tpmNome = tpmNome.substr(0,3)
-        }
-        setNome(tpmNome)
-       
+        setNome(event.target.value)
       }} id='nome' label='Nome' variant='outlined' fullWidth margin='normal'/>
       <TextField 
       value={sobrenome} onChange={(event)=>{
         setSobrenome(event.target.value)
       }} id='sobrenome' label='Sobrenome' variant='outlined' fullWidth margin='normal'/>
-      <TextField id='CPF' label='CPF' variant='outlined' fullWidth margin='normal'/>
+      <TextField value={cpf} onChange={(event)=>{
+        setCpf(event.target.value)
+      }} id='CPF' label='CPF' variant='outlined' fullWidth margin='normal'/>
       <FormControlLabel label='Promoções' control={<Switch defaultChecked color='primary' />}/>
       <FormControlLabel label='Novidades' control={<Switch defaultChecked color='primary' />}/>
       <Button type='submit' variant='contained' color='primary'>cadastrar</Button>
