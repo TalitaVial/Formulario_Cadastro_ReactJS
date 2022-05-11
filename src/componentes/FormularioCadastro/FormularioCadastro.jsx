7a4048320fd32ba1a4885e1f0538ad8b775fe34e
Talita Vial <talitavialsilva1@gmail.com>
2022-05-10 20:56:04 -0400

    feat: armazendado o estado dos campos promoções e novidades

--- src/componentes/FormularioCadastro/FormularioCadastro.jsx
+++ src/componentes/FormularioCadastro/FormularioCadastro.jsx
@@ -5,13 +5,13 @@ function FormularioCadastro() {
   const [nome, setNome] = useState("")
   const [sobrenome, setSobrenome] = useState("")
   const [cpf, setCpf] = useState("")
-  // const [promocoes, setPromocoes] = useState("")
-  // const [novidades, setNovidades] = useState("")
+  const [promocoes, setPromocoes] = useState(true)
+  const [novidades, setNovidades] = useState(true)
 
   return (
     <form onSubmit={(event)=>{
       event.preventDefault()
-      console.log(nome, sobrenome)
+      console.log({nome, sobrenome, cpf, promocoes, novidades})
     }}>
       <TextField value={nome} onChange={(event)=>{
         setNome(event.target.value)
@@ -23,8 +23,12 @@ function FormularioCadastro() {
       <TextField value={cpf} onChange={(event)=>{
         setCpf(event.target.value)
       }} id='CPF' label='CPF' variant='outlined' fullWidth margin='normal'/>
-      <FormControlLabel label='Promoções' control={<Switch defaultChecked color='primary' />}/>
-      <FormControlLabel label='Novidades' control={<Switch defaultChecked color='primary' />}/>
+      <FormControlLabel label='Promoções' control={<Switch checked={promocoes} onChange={(event)=>{
+        setPromocoes(event.target.checked)
+      }} color='primary' />}/>
+      <FormControlLabel label='Novidades' control={<Switch checked={novidades} onChange={(event)=>{
+        setNovidades(event.target.checked)
+      }} color='primary' />}/>
       <Button type='submit' variant='contained' color='primary'>cadastrar</Button>
     </form>
   );
